e0aa788e2a22a4919be1fe2c36d5a4480541c5e2
natemoo-re <ntmoore@knox.edu>
2018-05-31 08:44:51 -0500

    Initial commit

---
+++ stencil.config.js
@@ -0,0 +1,27 @@
+const sass = require('@stencil/sass');
+
+exports.config = {
+	plugins: [
+		sass({
+			injectGlobalPaths: [
+				'src/styles/sass-global.scss'
+			]
+		})
+	],
+	globalStyle: 'src/styles/global.scss',
+	copy: [
+		{ src: 'content' }
+	],
+	outputTargets: [
+		{
+			type: 'www',
+			dir: 'www/docs',
+			baseUrl: '/docs',
+		}
+	]
+};
+
+exports.devServer = {
+	root: 'www',
+	watchGlob: '**/**'
+};
